ed6120ee4e6f55b17bd8d38809a96b6fcd98d621
Sunitha1021 <ss8172127@gmail.com>
2024-11-04 12:24:51 +0530

    Add files via upload

---
+++ arthimetic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    int b=40;
+    int c=a+b;
+    int d=b-a;
+    int e=a*b;
+    int f=b/a;
+    int g=a%b;
+    printf("%d \n %d \n %d \n %d \n %d",c,d,e,f,g);
+    return 0;
+
+}
